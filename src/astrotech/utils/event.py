716f4863161fcb40b73631e66f9e29c456d17b1b
Thibaut Durand <durand.tibo@gmail.com>
2024-06-07 23:32:58 -0700

    Add custom events

---
+++ src/astrotech/utils/event.py
@@ -0,0 +1,84 @@
+r"""Contain utility functions to manage events."""
+
+from __future__ import annotations
+
+__all__ = ["EventHandler", "ConditionalEventHandler"]
+
+from coola.utils import str_indent, str_mapping
+from minevent import ConditionalEventHandler as ConditionalEventHandler_
+from minevent import EventHandler as EventHandler_
+
+
+class EventHandler(EventHandler_):
+    r"""Implements a variant of ``minvent.EventHandler`` to not show the
+    arguments in the to string method.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from astrotech.utils.event import EventHandler
+    >>> def hello_handler() -> None:
+    ...     print("Hello!")
+    ...
+    >>> handler = EventHandler(hello_handler)
+    >>> print(repr(handler))  # doctest:+ELLIPSIS
+    EventHandler(
+      (handler): <function hello_handler at 0x...>
+      (handler_args): ()
+      (handler_kwargs): {}
+    )
+    >>> print(str(handler))  # doctest:+ELLIPSIS
+    EventHandler(
+      (handler): <function hello_handler at 0x...>
+    )
+    >>> handler.handle()
+    Hello!
+
+    ```
+    """
+
+    def __str__(self) -> str:
+        args = str_indent(str_mapping({"handler": self._handler}))
+        return f"{self.__class__.__qualname__}(\n  {args}\n)"
+
+
+class ConditionalEventHandler(ConditionalEventHandler_):
+    r"""Implements a variant of ``minvent.ConditionalEventHandler`` to
+    not show the arguments in the to string method.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from minevent import PeriodicCondition
+    >>> from astrotech.utils.event import ConditionalEventHandler
+    >>> def hello_handler() -> None:
+    ...     print("Hello!")
+    ...
+    >>> handler = ConditionalEventHandler(hello_handler, PeriodicCondition(freq=3))
+    >>> print(repr(handler))  # doctest:+ELLIPSIS
+    ConditionalEventHandler(
+      (handler): <function hello_handler at 0x...>
+      (handler_args): ()
+      (handler_kwargs): {}
+      (condition): PeriodicCondition(freq=3, step=0)
+    )
+    >>> print(str(handler))  # doctest:+ELLIPSIS
+    ConditionalEventHandler(
+      (handler): <function hello_handler at 0x...>
+      (condition): PeriodicCondition(freq=3, step=0)
+    )
+    >>> handler.handle()
+    Hello!
+    >>> handler.handle()
+    >>> handler.handle()
+    >>> handler.handle()
+    Hello!
+
+    ```
+    """
+
+    def __str__(self) -> str:
+        args = str_indent(str_mapping({"handler": self._handler, "condition": self._condition}))
+        return f"{self.__class__.__qualname__}(\n  {args}\n)"
