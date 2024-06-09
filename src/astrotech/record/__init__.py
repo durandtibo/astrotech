a8014e98bc14fefea59567fcd56eb93bc59210d6
Thibaut Durand <durand.tibo@gmail.com>
2024-06-08 23:50:54 -0700

    Add comparators (#19)

--- src/astrotech/record/__init__.py
+++ src/astrotech/record/__init__.py
@@ -2,7 +2,19 @@ r"""Contain the record implementations."""
 
 from __future__ import annotations
 
-__all__ = ["BaseRecord", "get_max_size", "set_max_size"]
+__all__ = [
+    "BaseComparator",
+    "BaseRecord",
+    "MaxScalarComparator",
+    "MinScalarComparator",
+    "get_max_size",
+    "set_max_size",
+]
 
 from astrotech.record._config import get_max_size, set_max_size
 from astrotech.record.base import BaseRecord
+from astrotech.record.comparator import (
+    BaseComparator,
+    MaxScalarComparator,
+    MinScalarComparator,
+)
