c2a628da342d25fa001599258eca02b286d35dbe
Thibaut Durand <durand.tibo@gmail.com>
2024-06-08 17:19:25 -0700

    Add functions to configure records

--- src/astrotech/record/__init__.py
+++ src/astrotech/record/__init__.py
@@ -1,1 +1,8 @@
 r"""Contain the record implementations."""
+
+from __future__ import annotations
+
+__all__ = ["BaseRecord", "get_max_size", "set_max_size"]
+
+from astrotech.record._config import get_max_size, set_max_size
+from astrotech.record.base import BaseRecord
